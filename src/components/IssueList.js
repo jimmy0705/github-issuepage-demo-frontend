23e583737cec7abc84e2001ddb38ef5c8c7387b6
juned123 <juned@frrole.com>
2020-08-24 16:49:22 +0530

    posting form data

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -5,7 +5,7 @@ import IssueCard from './IssueCard'
 import { Card,Row,Col } from 'react-bootstrap';
 
 function IssueList({issues}) {
-  console.log(issues)
+ // console.log(issues)
   return (
     <div className="Issue-list">
         {issues.map(issue=><IssueCard issue={issue} key={issue._id}/>)}
